2d34abe2da340568b80a581626d9e3233f4f71d0
Iwo <g00391231@atu.ie>
2023-11-08 09:49:03 +0000

    Setup

---
+++ src/test/java/ie/atu/exam2/Exam2ApplicationTests.java
@@ -0,0 +1,13 @@
+package ie.atu.exam2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Exam2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
